af525e2d860069db02ecbf8d7d1aa80718c7f782
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-05-30 22:15:24 -0400

    update confirmation window to use tailwind modal

--- src/lib/components/RowTableReminder.svelte
+++ src/lib/components/RowTableReminder.svelte
@@ -112,37 +112,24 @@
 		/>
 	{/if}
 	{#if isConfirmationModalVisible}
-		<div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-			<div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-				<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-				<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-				<div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-					<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-						<div class="sm:flex sm:items-start">
-							<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-								<h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-									Confirmar eliminación
-								</h3>
-								<div class="mt-2">
-									<p class="text-sm text-gray-500">
-										¿Estás seguro de que quieres eliminar esta cita?
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-						<button type="button" class="delete-btn" on:click={() => { deleteAppointment(id_appointment); isConfirmationModalVisible = false; }}>
-							Confirmar
-						</button>
-						<button type="button" class="my-button" on:click={() => { isConfirmationModalVisible = false; }}>
-							Cancelar
-						</button>
-					</div>
+	<div class="fixed inset-0 flex items-center justify-center z-50">
+		<div class="modal modal-open">
+			<div class="modal-box">
+				<h2 class="text-xl">Confirmar eliminación</h2>
+				<p>¿Estás seguro de que quieres eliminar esta cita?</p>
+				<div class="modal-action">
+					<button class="btn" on:click={() => { isConfirmationModalVisible = false; }}>
+						Cancelar
+					</button>
+					<button class="btn btn-error" on:click={() => { deleteAppointment(id_appointment); isConfirmationModalVisible = false; }}>
+						Confirmar
+					</button>
 				</div>
 			</div>
 		</div>
-	{/if}
+	</div>
+{/if}
+
 </tr>
 
 <style>
