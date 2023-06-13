f85488e6beb9c55d64c8ab3e1149a3a75d91714f
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-06-13 00:11:56 -0400

    Added logic to extract doctor and user appointment information

--- src/routes/api/appoinments/get-all/+server.ts
+++ src/routes/api/appoinments/get-all/+server.ts
@@ -2,18 +2,41 @@ import { json } from '@sveltejs/kit';
 import type { RequestEvent, RequestHandler } from './$types';
 import { prisma } from '$lib/database/prisma';
 
-export const GET: RequestHandler = async ({ url }: RequestEvent) => {
-	const idUser = url.searchParams.get('key') ?? 0;
-	if (idUser == 0) {
-		return json([]);
-	}
+type CustomRouteParams = {
+	isDoctor: string;
+};
 
-	const results = await prisma.appointment_form.findMany({
-		where: {
-			id_user: idUser
-		}
-	});
-
-	await prisma.$disconnect();
-	return json(results);
+  
+export const GET: RequestHandler<CustomRouteParams> = async ({ url, params }: RequestEvent<CustomRouteParams>) => {
+	console.log(url.searchParams.get('isDoctor'))
+	const isDoctor = url.searchParams.get('isDoctor')==='true'
+  	let results;
+  if (isDoctor) {
+    const idDoctor = url.searchParams.get('key') ?? '0';
+    if (idDoctor === '0') {
+      return json([]);
+    }
+    
+    results = await prisma.appointment_form.findMany({
+      where: {
+        id_doctor: idDoctor
+      }
+    });
+    await prisma.$disconnect();
+    return json(results);
+  } else {
+    const idUser = url.searchParams.get('key') ?? '0';
+    if (idUser === '0') {
+      return json([]);
+    }
+    
+    results = await prisma.appointment_form.findMany({
+      where: {
+        id_user: idUser
+      }
+    });
+    
+    await prisma.$disconnect();
+    return json(results);
+  }
 };
