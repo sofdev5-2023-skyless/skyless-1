420775b2f568688752b03bf6486f3db38bce8ab9
Ariel  Claros <ariel.claros.60091@usalesiana.edu.bo>
2023-06-16 15:55:47 -0400

    ErrorZod created for ReminderForm file

---
+++ src/lib/components/ErrorZod.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let isError = false;
+	export let description = '';
+</script>
+
+{#if isError}
+	<label for="description" class="label">
+		<span class="label-text-alt text-red-500">{description}</span>
+	</label>
+{/if}
