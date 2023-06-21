25e97c5c4c5bd5605895ec192f898a3980b854d1
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-06-21 01:50:27 -0400

    Added Homwdoctro chedule time control funtcion

--- src/lib/components/HomeDoctor.svelte
+++ src/lib/components/HomeDoctor.svelte
@@ -3,8 +3,13 @@
 	import Carousel from './Carousel.svelte';
 	import { areYouDoctor, masterKey } from '$lib/stores/store';
 	import ReminderTable from './DoctorReminderTable.svelte';
+	import ScheduleForm from "./ScheduleForm.svelte";
 	import { goto } from '$app/navigation';
-
+	let isFormVisible = false;
+  
+  	function toggleForm() {
+    	isFormVisible = !isFormVisible;
+  	}
 	onMount(async () => {
 		const resp = await fetch(`/api/doctors/read?id=${$masterKey}`);
 		const js = await resp.json();
@@ -19,6 +24,10 @@
 <Carousel />
 
 <div class="p-10 sm:ml-614">
+	<button class="btn btn-primary" >SCHEDULE APPOINTMENT TIME</button>
+	{#if isFormVisible}
+    <ScheduleForm on:close={toggleForm} />
+  {/if}
 	<div class="overflow-x-auto w-full">
 		<ReminderTable />
 	</div>
