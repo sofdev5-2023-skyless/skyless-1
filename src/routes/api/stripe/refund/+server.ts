f78e651eef115b2389850aff09b293b152378190
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-06-27 13:11:34 -0400

    Added api for refunds

---
+++ src/routes/api/stripe/refund/+server.ts
@@ -0,0 +1,22 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler, RequestEvent } from '../../login/$types';
+import Stripe from 'stripe';
+
+const key =
+'sk_test_51NMtdHIqt903VK65FXYQmEuVFAQfAwmS5c8fajrCIgbcWP1tIjyhodo2gXVTalDjyPhWMxk2zgR61ikQJG40SrqV00uoDxU814';
+
+const stripe = new Stripe(key, {
+    apiVersion: '2022-11-15'
+});
+
+export const POST: RequestHandler = async ({ request }: RequestEvent) => {
+    const { id_payment } = await request.json();
+
+    const refund = await stripe.refunds.create({
+        payment_intent: id_payment,
+    });
+
+    console.log(refund);
+
+    return json(refund);
+};
