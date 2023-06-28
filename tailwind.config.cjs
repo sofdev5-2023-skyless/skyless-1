9443cf69dea8fae8543a46bc456b2e49b60cb65c
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-06-28 11:50:41 -0400

    Formatted tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,21 +6,21 @@ const config = {
 
 	daisyui: {
 		themes: [
-		  {
-			mytheme: {
-   				"primary": "#d1c1d7",
-   				"secondary": "#f6cbd1",
-   				"accent": "#b4e9d6",
-   				"neutral": "#70acc7",
-   				"base-100": "#ffffff",
-   				"info": "#3abff8",
-   				"success": "#36d399",
-   				"warning": "#fbbd23",
-				"error": "#f87272",
-			},
-		  },
-		],
-	  },
+			{
+				mytheme: {
+					primary: '#d1c1d7',
+					secondary: '#f6cbd1',
+					accent: '#b4e9d6',
+					neutral: '#70acc7',
+					'base-100': '#ffffff',
+					info: '#3abff8',
+					success: '#36d399',
+					warning: '#fbbd23',
+					error: '#f87272'
+				}
+			}
+		]
+	},
 
 	plugins: [require('daisyui'), require('flowbite/plugin')],
 	daisyui: {
