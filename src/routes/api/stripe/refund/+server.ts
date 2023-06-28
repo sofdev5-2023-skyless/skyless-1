a18c7b436cc3efdef10fd11b42ad6f9dba897c8c
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-06-27 23:51:17 -0400

    Use the private key of stripe from the .env

--- src/routes/api/stripe/refund/+server.ts
+++ src/routes/api/stripe/refund/+server.ts
@@ -1,22 +1,20 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler, RequestEvent } from '../../login/$types';
 import Stripe from 'stripe';
+import { env } from '$env/dynamic/private';
 
-const key =
-'sk_test_51NMtdHIqt903VK65FXYQmEuVFAQfAwmS5c8fajrCIgbcWP1tIjyhodo2gXVTalDjyPhWMxk2zgR61ikQJG40SrqV00uoDxU814';
-
-const stripe = new Stripe(key, {
-    apiVersion: '2022-11-15'
+const stripe = new Stripe(env.PRIVATE_KEY_STRIPE, {
+	apiVersion: '2022-11-15'
 });
 
 export const POST: RequestHandler = async ({ request }: RequestEvent) => {
-    const { payment_intent } = await request.json();
+	const { payment_intent } = await request.json();
 
-    const refund = await stripe.refunds.create({
-        payment_intent: payment_intent
-    });
+	const refund = await stripe.refunds.create({
+		payment_intent: payment_intent
+	});
 
-    console.log(refund);
+	console.log(refund);
 
-    return json(refund);
+	return json(refund);
 };
