a28e5e6ea953d257a3fda2f83609b2488d643d69
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-06-28 14:19:51 -0400

    Update logic to work with the new table

--- data-base/postgres/sql/V1_14__schedule_table_create.sql
+++ data-base/postgres/sql/V1_14__schedule_table_create.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS bd_schema.schedule (
-    id_schedule int PRIMARY KEY,
+    id_schedule TEXT PRIMARY KEY,
 	schedule VARCHAR(255)
 ) TABLESPACE pg_default;
--- src/routes/api/doctor_schedule/read-doctor/+server.ts
+++ src/routes/api/doctor_schedule/read-doctor/+server.ts
@@ -1,17 +1,35 @@
 import { json } from '@sveltejs/kit';
 import type { RequestEvent, RequestHandler } from './$types';
 import { prisma } from '$lib/database/prisma';
+import type { doctor_schedule } from '@prisma/client';
 
 export const GET: RequestHandler = async ({ url }: RequestEvent) => {
 	const id = url.searchParams.get('id') ?? '0';
 
 	const result = await prisma.doctor_schedule.findMany({
-		where: { id_doctor: id }
+		where: { id_doctor: id },
+		select: {
+			id: true,
+			id_doctor: true,
+			occupied: true,
+			schedule_doctor_schedule_scheduleToschedule: {
+				select: {
+					schedule: true
+				}
+			}
+		}
 	});
+
+	const resultParsed: doctor_schedule[] = result.map((elem) => ({
+		id: elem.id,
+		id_doctor: elem.id_doctor,
+		occupied: elem.occupied,
+		schedule: elem.schedule_doctor_schedule_scheduleToschedule.schedule
+	}));
 
 	await prisma.$disconnect();
 	if (result != null) {
-		return json(result);
+		return json(resultParsed);
 	}
 	return json({ ok: 400 });
 };
