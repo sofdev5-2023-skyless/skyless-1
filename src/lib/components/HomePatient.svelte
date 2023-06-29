9627b4603654ff952479e88d81dd8dfdaf84de06
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-06-29 16:56:18 -0400

    Update css to center elements in the page

--- src/lib/components/HomePatient.svelte
+++ src/lib/components/HomePatient.svelte
@@ -29,38 +29,37 @@
 	];
 </script>
 
-<Carousel />
-{#if $menuOpen}
-	<SideMenu />
-{/if}
-<div class="grid grid-cols-2 md:grid-cols-3 gap-4 p-20 sm:ml-64">
-	{#each specialities as { Speciality, Description, Path }, index}
-		<div
-			aria-posinset={index + 1}
-			aria-setsize={specialities.length}
-			class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-		>
-			<a href="#">
-				<img class="h-auto max-w-full rounded-lg" src={items[index]} alt={Path} />
-			</a>
-			<div class="p-5">
+<div class="container mx-auto">
+	<Carousel />
+	{#if $menuOpen}
+		<SideMenu />
+	{/if}
+	<div class="grid md:grid-cols-3 gap-3 p-20">
+		{#each specialities as { Speciality, Description, Path }, index}
+			<div
+				aria-posinset={index + 1}
+				aria-setsize={specialities.length}
+				class="flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+			>
 				<a href="#">
-					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-						{Speciality}
-					</h5>
+					<img class="h-auto max-w-full rounded-lg" src={items[index]} alt={Path} />
 				</a>
-				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{Description}</p>
-				<a
-					href="#"
-					class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>
-					See more
-					<ArrowStick />
-				</a>
+				<div class="p-5">
+					<a href="#">
+						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							{Speciality}
+						</h5>
+					</a>
+					<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{Description}</p>
+					<a href="#" class="btn btn-primary">
+						See more
+						<ArrowStick />
+					</a>
+				</div>
 			</div>
-		</div>
-	{/each}
+		{/each}
 
-	<!-- Put this part before </body> tag -->
-	<Register />
+		<!-- Put this part before </body> tag -->
+		<Register />
+	</div>
 </div>
