56895fcf213f4f2eda7ff9bf880c3d38603afda5
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-06-30 00:34:49 -0400

    Added renders the doctor name correctly

--- tests/lib/components/DoctorCard.test.ts
+++ tests/lib/components/DoctorCard.test.ts
@@ -2,28 +2,18 @@ import { cleanup, render, screen } from '@testing-library/svelte';
 import DoctorCard from '../../../src/lib/components/DoctorCard.svelte';
 
 describe('DoctorCard component', () => {
-	it('should render the correct title', () => {
-		const { getByText } = render(DoctorCard, {
-			props: {
-				name: 'John',
-				lastName: 'Doe'
-			}
-		});
+	it('renders the doctor name correctly', () => {
+		const props = {
+		  id: '1',
+		  name: 'John',
+		  lastName: 'Doe',
+		  speciality: 'Cardiology',
+		  cellphone: '123456789',
+		};
+	  
+		const { getByText } = render(DoctorCard, props);
+		expect(getByText('John Doe')).toBeInTheDocument();
+	  });
+	  
 
-		expect(getByText('John Doe')).toBeTruthy();
-	});
-
-	it('should render the correct link', () => {
-		const { getByText, getByTestId } = render(DoctorCard, {
-			props: {
-				cellphone: '123-456-7890'
-			}
-		});
-		expect(getByText('123-456-7890')).toBeTruthy();
-	});
-	it('should render the correct link', () => {
-		const { getByText } = render(DoctorCard);
-
-		expect(getByText('More information')).toBeTruthy();
-	});
 });
