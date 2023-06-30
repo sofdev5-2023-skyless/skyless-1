1b72edf26a236dba8407e4b7d682d73e97b20c01
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-06-30 00:34:49 -0400

    Added renders the doctor name correctly

--- tests/lib/components/DoctorCard.test.ts
+++ tests/lib/components/DoctorCard.test.ts
@@ -2,69 +2,56 @@ import { cleanup, render, screen } from '@testing-library/svelte';
 import DoctorCard from '../../../src/lib/components/DoctorCard.svelte';
 
 describe('DoctorCard component', () => {
-	it('renders the doctor name correctly', () => {
+	it('renders the doctor speciality correctly', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
-		const { getByText } = render(DoctorCard, props);
-		expect(getByText('John Doe')).toBeInTheDocument();
-	  });
-	  
-	  it('renders the doctor speciality correctly', () => {
-		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
-		};
-	  
+
 		const { getByText } = render(DoctorCard, props);
 		expect(getByText('Cardiology')).toBeInTheDocument();
-	  });
-	  
+	});
+
 	it('renders the doctor cellphone correctly', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
+
 		const { getByText } = render(DoctorCard, props);
 		expect(getByText('123456789')).toBeInTheDocument();
-	  });
-	  
+	});
+
 	it('renders the default image correctly', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
+
 		const { getByAltText } = render(DoctorCard, props);
 		expect(getByAltText('default')).toBeInTheDocument();
-	  });
-	  
+	});
+
 	it('renders the doctor information correctly', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
+
 		const { getByText, getByAltText } = render(DoctorCard, props);
-	  	expect(getByText('John Doe')).toBeInTheDocument();
+		expect(getByText('John Doe')).toBeInTheDocument();
 		expect(getByText('Cardiology')).toBeInTheDocument();
 		expect(getByText('123456789')).toBeInTheDocument();
 		expect(getByAltText('default')).toBeInTheDocument();
@@ -82,25 +69,24 @@ describe('DoctorCard component', () => {
 
 	it('should render the updated name and last name when props are changed', () => {
 		const { getByText, rerender } = render(DoctorCard, {
-		  props: {
-			name: 'Jane',
-			lastName: 'Smith'
-		  }
+			props: {
+				name: 'Jane',
+				lastName: 'Smith'
+			}
 		});
-	  
+
 		expect(getByText('Jane Smith')).toBeTruthy();
-	  
+
 		rerender({
-		  props: {
-			name: 'Mark',
-			lastName: 'Johnson'
-		  }
+			props: {
+				name: 'Mark',
+				lastName: 'Johnson'
+			}
 		});
-	  
+
 		expect(getByText('Mark Johnson')).toBeTruthy();
-	  });
-	
-	
+	});
+
 	it('should not render the link when `cellphone` prop is not provided', () => {
 		const { queryByText } = render(DoctorCard);
 		expect(queryByText('123-456-7890')).toBeNull();
@@ -108,13 +94,13 @@ describe('DoctorCard component', () => {
 
 	it('should render the correct link with different valid `cellphone` values', () => {
 		const { getByText } = render(DoctorCard, {
-		  props: {
-			cellphone: '111-222-3333'
-		  }
+			props: {
+				cellphone: '111-222-3333'
+			}
 		});
 		expect(getByText('111-222-3333')).toBeTruthy();
 	});
-	
+
 	it('should render the correct link', () => {
 		const { getByText, getByTestId } = render(DoctorCard, {
 			props: {
@@ -126,57 +112,57 @@ describe('DoctorCard component', () => {
 
 	it('displays the ReminderForm component', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
+
 		const { getByText } = render(DoctorCard, props);
-	  
+
 		expect(getByText('Book Medical Appointment')).toBeInTheDocument();
 	});
-	
+
 	it('displays the "Fecha:" text', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
+
 		const { getByText } = render(DoctorCard, props);
-	  
+
 		expect(getByText('Fecha:')).toBeInTheDocument();
-	  });
-	
+	});
+
 	it('displays the "Schedule" text', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
+
 		const { getByText } = render(DoctorCard, props);
-	  
+
 		expect(getByText('Schedule')).toBeInTheDocument();
 	});
-	
+
 	it('displays whole informtation at the ReminderForm component', () => {
 		const props = {
-		  id: '1',
-		  name: 'John',
-		  lastName: 'Doe',
-		  speciality: 'Cardiology',
-		  cellphone: '123456789',
+			id: '1',
+			name: 'John',
+			lastName: 'Doe',
+			speciality: 'Cardiology',
+			cellphone: '123456789'
 		};
-	  
+
 		const { getByText } = render(DoctorCard, props);
-	  	expect(getByText('Book Medical Appointment')).toBeInTheDocument();
+		expect(getByText('Book Medical Appointment')).toBeInTheDocument();
 		expect(getByText('Fecha:')).toBeInTheDocument();
 		expect(getByText('Schedule')).toBeInTheDocument();
 	});
@@ -186,6 +172,4 @@ describe('DoctorCard component', () => {
 
 		expect(getByText('More information')).toBeTruthy();
 	});
-
-	
 });
