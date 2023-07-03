fbfa79d472f4e4cf7553ee9162907cc849502a59
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-07-03 00:11:26 -0400

    Home patient modified

--- src/lib/components/HomePatient.svelte
+++ src/lib/components/HomePatient.svelte
@@ -9,10 +9,7 @@
 	import traumatologia from '$lib/images/especialidades/traumatologia.png';
 	import otorrinolaringologia from '$lib/images/especialidades/otorrinolaringologia.png';
 	import cirugiaplastica from '$lib/images/especialidades/cirugiaplastica.png';
-	import SideMenu from '$lib/components/SideMenu.svelte';
-	import { menuOpen } from '$lib/stores/store';
 	import specialities from '$lib/data/specialities.json';
-	import Register from './Register.svelte';
 
 	import Carousel from './Carousel.svelte';
 
@@ -41,16 +38,19 @@
 				class="flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 				data-aos="fade-up"
 			>
+				<!-- svelte-ignore a11y-invalid-attribute -->
 				<a href="#">
 					<img class="h-auto max-w-full rounded-lg" src={items[index]} alt={Path} />
 				</a>
 				<div class="p-5">
+					<!-- svelte-ignore a11y-invalid-attribute -->
 					<a href="#">
 						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 							{Speciality}
 						</h5>
 					</a>
 					<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{Description}</p>
+					<!-- svelte-ignore a11y-invalid-attribute -->
 					<a href="#" class="btn btn-primary">
 						See more
 						<ArrowStick />
